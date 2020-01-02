feb78b038a412f6dd5123f5f05118905ad5ecab0
farshid <farshidboroomand@gmail.com>
2020-01-02 20:06:55 +0330

    userDomain and channel routes added

---
+++ src/Config/Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Arvan\Vod\Config;
+
+class Routes
+{
+    const CREATE_USER_DOMAIN = '/domains';
+    const GET_USER_DOMAIN = '/domains';
+
+    const GET_CHANNELS = '/channels';
+    const GET_CHANNEL = '/channels/:channel_id';
+    const CREATE_CHANNEL = '/channels';
+    const UPDATE_CHANNEL = '/channels/:channel_id';
+    const DELETE_CHANNEL = '/channels/:channel_id';
+}
